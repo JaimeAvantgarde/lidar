6370d92b6a4e2d69bcb695771132519b7562deae
Jaime.pareja <141130879+JaimeAvantgarde@users.noreply.github.com>
2026-02-09 15:30:44 +0100

    Initial Commit

---
+++ lidar/lidarApp.swift
@@ -0,0 +1,17 @@
+//
+//  lidarApp.swift
+//  lidar
+//
+//  Created by Jaime Pareja Arco on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct lidarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lidarTests/lidarTests.swift
@@ -0,0 +1,16 @@
+//
+//  lidarTests.swift
+//  lidarTests
+//
+//  Created by Jaime Pareja Arco on 9/2/26.
+//
+
+import Testing
+
+struct lidarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
